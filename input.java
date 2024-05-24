aa73a9525e47f5970a3de657ef789bfe607bb266
shivpatel027 <patelshiv1701@gmail.com>
2024-05-23 22:03:04 -0500

    java basics added

---
+++ input.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class input {
+    public static void main(String[] args) {
+        //Input
+        Scanner sc = new Scanner(System.in);
+        String name = sc.next();
+        System.out.println(name);
+    }
+}
---
+++ sum.java
@@ -0,0 +1,10 @@
+public class sum {
+    public static void main(String args[]) {
+        int a = 10;
+        int b = 23;
+        int ans = a + b;
+        System.out.println(ans);
+        int diff = b - a;
+        System.out.println(diff); 
+    }
+}
